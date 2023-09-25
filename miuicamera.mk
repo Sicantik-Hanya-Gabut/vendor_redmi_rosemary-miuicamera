3bb383ed9ffe4deedd3165d23f70a053e0272285
Beru Krugermeier <windowz414@gnuweeb.org>
2023-09-25 16:16:14 +0300

    camera: Initial commit
    Signed-off-by: Beru Krugermeier <windowz414@gnuweeb.org>

---
+++ miuicamera.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/camera/configs/permissions/default-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions-miuicamera.xml \
+    vendor/xiaomi/camera/configs/permissions/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml \
+    vendor/xiaomi/camera/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Device-Features
+PRODUCT_COPY_FILES += \
+     vendor/xiaomi/camera/configs/device_features/rosemary.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/rosemary.xml \
+     vendor/xiaomi/camera/configs/device_features/secret.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/secret.xml \
+     vendor/xiaomi/camera/configs/device_features/maltose.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/maltose.xml \
+     vendor/xiaomi/camera/configs/device_features/rosemary.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/rosemary.xml \
+     vendor/xiaomi/camera/configs/device_features/secret.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/secret.xml \
+     vendor/xiaomi/camera/configs/device_features/maltose.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/munch.xml
+
+# Properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.com.google.lens.oem_camera_package=com.android.camera \
+    ro.miui.notch=1 \
+    ro.product.mod_device=rosemary
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/xiaomi/camera/sepolicy/vendor
+
+$(call inherit-product, vendor/xiaomi/camera/camera-vendor.mk)
